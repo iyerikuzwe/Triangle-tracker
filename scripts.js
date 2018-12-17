3568ec9e793d32d0b09295aa8520639c9e1f45fe
iyerikuzwe regine <iyerikuzweregine19@gmail.com>
2018-12-17 08:21:48 +0200

    add changes into scripts.js

--- scripts.js
+++ scripts.js
@@ -1,17 +1,22 @@
-function getResult() {
-    var hypotenuse = parseFloat(prompt("side hypotenuse"));
-    var opposite = parseFloat(prompt("side opposite"));
-    var adjacent = parseFloat(prompt("side adjacent"));
-    if (hypotenuse === opposite && hypotenuse === hypotenuse && opposite === adjacent) {
-        alert("Equilateral");
+function getResult(){
+    var side1 = parseFloat(document.getElementById('side1').value);
+    var side2 = parseFloat(document.getElementById('side2').value);
+    var side3 = parseFloat(document.getElementById('side3').value);
+    
+    if(side1 === side2 && side2 === side3){
+        alert("This is an equilateral triangle ");
+    window.location.href="eqlter.html";
     }
-    else if (hypotenuse === opposite || hypotenuse === adjacent || opposite === adjacent) {
-        alert("Isosceles");
+    else if(side1 === side2 || side1 === side3 || side2 === side3){
+    alert("This is an isosceles triangle ");
+    window.location.href="isoceles.html";
     }
-    else if ((hypotenuse + opposite) <= adjacent || (hypotenuse + adjacent) <= opposite || (opposite + adjacent) <= hypotenuse) {
-        alert("Not a definite triangle");
+    else if((side1 + side2) <= side3 || (side1 + side3) <= side2 || (side2 + side3) <= side1) {
+    alert("Not a definite triangle");
+    window.location.href="undefined-triangle.html";
     }
-    else {
-        alert("Scalene");
+    else{
+    alert("This is a scalene triangle");
+    window.location.href="scalene.html";
     }
-}
+    }
